607f93ba85c047a9b312ba627c35d69167ca849a
Bryan Vossen <bryan.vossen@gmail.com>
2025-02-17 15:33:40 -0700

    revision 1

--- bryans_setups/grating_tilter.py
+++ bryans_setups/grating_tilter.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+import time
 
 from raypier.tracer import RayTraceModel
 from raypier.faces import CylindericalFace, PlanarFace, SphericalFace
@@ -8,7 +11,7 @@ from raypier.diffraction_gratings import RectangularGrating
 from raypier.mirrors import BaseMirror, PECMirror, RectMirror
 from raypier.sources import BroadbandRaySource
 from raypier.beamstop import BeamStop
-from raypier.results import GroupVelocityDispersion
+from raypier.results import GroupVelocityDispersion, MeanOpticalPathLength, evaluate_phase
 from raypier.constraints import BaseConstraint
 from raypier.chirp_result import ChirpResult
 from raypier.materials import OpticalMaterial
@@ -22,15 +25,25 @@ from traits.api import Range, on_trait_change
 from traitsui.api import View, Item
 
 
-direction = np.array([0.0,0.0,0.5]) - np.array([-29.685957,124.73850,10])
+# direction = np.array([0.0,0.0,0.5]) - np.array([-29.685957,124.73850,10])
 
-source = BroadbandRaySource(origin=(-29.685957,124.73850,10),
+# source = BroadbandRaySource(origin=(-29.685957,124.73850,10),
+#                             direction=tuple(direction),
+#                             wavelength_start=0.78,
+#                             wavelength_end = 0.82,
+#                             number=260,
+#                             uniform_deltaf=True,
+#                             max_ray_len=300.0)
+
+direction = np.array([0.0,-1,-0.1])
+
+source = BroadbandRaySource(origin=(0,70,10),
                             direction=tuple(direction),
-                            wavelength_start=0.76,
-                            wavelength_end = 0.84,
+                            wavelength_start=0.78,
+                            wavelength_end = 0.82,
                             number=260,
                             uniform_deltaf=True,
-                            max_ray_len=300.0)
+                            max_ray_len=800.0)
 
 # source = BroadbandGaussletSource(origin=(-29.685957,124.73850,10),
 #                             direction=tuple(direction),
@@ -54,13 +67,13 @@ source = BroadbandRaySource(origin=(-29.685957,124.73850,10),
 #                                )
 
 
-grating = RectangularGrating(centre=(10,50,-10),
+grating = RectangularGrating(centre=(0,46,-2),
                              direction=(-1,0,0),
-                             lines_per_mm=1400,
+                             lines_per_mm=1200,
                              order=-1,
-                             width = 15)
+                             width = 10)
 
-grating_init_rotation = -157.8
+grating_init_rotation = -151
 grating.orientation = grating_init_rotation
 
 lens = EdmundOptic45805(centre=(0,75,0),
@@ -102,8 +115,10 @@ mir = GeneralLens(name = "Cylinderincal Lens",
 #                   width=6.0,
 #                   length=10.0)
 
-bs = BeamStop(centre=(-15,75,-15),
-              direction=(-0.2314983 ,  0.97131408,  0.05438228))
+bs = BeamStop(centre=(0,50,-10),
+              direction=(0 , -1,  0),
+              diameter = 10
+              )
 
 gvd = GroupVelocityDispersion(source=source, target=bs.faces.faces[0])
 
@@ -141,15 +156,30 @@ gvd_cstr = GVDConstraint()
 
 chirp = ChirpResult(source=source, target=bs.faces.faces[0])
 
-# model = RayTraceModel(optics=[grating, lens, mir, bs], 
-#                       sources=[source,],
-#                       results=[gvd, chirp],
-#                       constraints=[gvd_cstr,])
+mean_optical_path = MeanOpticalPathLength(source = source, target = bs.faces.faces[0])
 
 model = RayTraceModel(optics=[ grating, mir, bs], 
                       sources=[source,],
-                      results=[gvd, chirp],
+                      results=[gvd, chirp, mean_optical_path],
                     #   constraints=[gvd_cstr,]
                         )
+
+try:
+    freq, phase = evaluate_phase(all_wavelengths=np.asarray(source.wavelength_list), traced_rays = source.traced_rays, target_face = bs.faces.faces[0])
+    wavelength = 299792.458 / freq 
+
+    fig, ax = plt.subplots()
+    ax.plot(wavelength, phase)
+    ax.set(xlabel = "Wavelength (nm)", ylabel = "Phase", title = "Phase Evalution")
+    ax.grid()
+
+
+    # Get current date and time
+    now = time.strftime("%Y%m%d-%H%M%S")
+    fig.savefig(f"D:\senior_design\Screenshots\phase_plots\phase_plot_{now}.png")
+except:
+    print("Error making plot")
+
+
 
 model.configure_traits(kind="live")

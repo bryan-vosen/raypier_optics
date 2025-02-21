3f87b2d0d167a48068999dd1909ca7badd00bbb2
Bryan Vossen <bryan.vossen@gmail.com>
2025-02-21 12:12:53 -0700

    made initial 500mm mirror

--- bryans_setups/grating_tilter.py
+++ bryans_setups/grating_tilter.py
@@ -11,7 +11,7 @@ from raypier.diffraction_gratings import RectangularGrating
 from raypier.mirrors import BaseMirror, PECMirror, RectMirror
 from raypier.sources import BroadbandRaySource
 from raypier.beamstop import BeamStop
-from raypier.results import GroupVelocityDispersion, MeanOpticalPathLength, evaluate_phase
+from raypier.results import GroupVelocityDispersion, MeanOpticalPathLength, evaluate_phase, evaluate_group_delay
 from raypier.constraints import BaseConstraint
 from raypier.chirp_result import ChirpResult
 from raypier.materials import OpticalMaterial
@@ -25,15 +25,23 @@ from traits.api import Range, on_trait_change
 from traitsui.api import View, Item
 
 
-direction = np.array([0.0,-1,-0.1])
+direction = np.array([0.0,-1,-0.015])
+grating_distance = 500
+grating_init_rotation = -151.31
+cylindrical_radius_curvature = 1000
+source_bandwidth = 32
+source_wavelength = 800
 
-source = BroadbandRaySource(origin=(0,70,10),
+
+wavelength_start = source_wavelength / 1000 - source_bandwidth/2000
+wavelength_end = source_wavelength / 1000 + source_bandwidth/2000
+source = BroadbandRaySource(origin=(0,260,10),
                             direction=tuple(direction),
-                            wavelength_start=0.78,
-                            wavelength_end = 0.82,
-                            number=260,
+                            wavelength_start=wavelength_start,
+                            wavelength_end = wavelength_end,
+                            number=500,
                             uniform_deltaf=True,
-                            max_ray_len=800.0)
+                            max_ray_len= 2000.0)
 
 # source = BroadbandGaussletSource(origin=(-29.685957,124.73850,10),
 #                             direction=tuple(direction),
@@ -56,14 +64,15 @@ source = BroadbandRaySource(origin=(0,70,10),
 #                                opacity=0.2
 #                                )
 
-grating_distance = 45.997
-grating = RectangularGrating(centre=(0,grating_distance,-2),
+
+grating = RectangularGrating(centre=(0,grating_distance,-4),
                              direction=(-1,0,0),
                              lines_per_mm=1200,
                              order=-1,
-                             width = 10)
+                             width = 20,
+                             length = 30)
 
-grating_init_rotation = -151
+
 grating.orientation = grating_init_rotation
 
 lens = EdmundOptic45805(centre=(0,75,0),
@@ -73,8 +82,8 @@ init_lens_rotation = lens.orientation
 
 shape = RectangleShape(centre = (0, 0), width = 80, height = 60)
 
-face1 = PlanarFace(z_height=0.0)
-face2 = CylindericalFace(z_height=4.0, curvature=100.0, mirror = True)
+#face1 = PlanarFace(z_height=0.0)
+face2 = CylindericalFace(z_height=4.0, curvature=cylindrical_radius_curvature, mirror = True)
 
 mat = OpticalMaterial(refractive_index=1)
 
@@ -83,15 +92,20 @@ mir = GeneralLens(name = "Cylinderincal Lens",
                      centre = (0,0,0),
                      direction=(0,-1,0),
                      shape=shape, 
-                     surfaces=[face2, 
-                               face1], 
+                     surfaces=[face2], 
                      materials=[mat])
 
 
-bs = BeamStop(centre=(0,50,-10),
+# bs = BeamStop(centre=(0,grating_distance+60,-20),
+#               direction=(0 , -1,  0),
+#               diameter = 100
+#               )
+
+bs = BeamStop(centre=(0,200,-27.5),
               direction=(0 , -1,  0),
-              diameter = 10
+              diameter = 40
               )
+
 
 gvd = GroupVelocityDispersion(source=source, target=bs.faces.faces[0])
 
@@ -139,17 +153,31 @@ model = RayTraceModel(optics=[ grating, mir, bs],
 
 try:
     freq, phase = evaluate_phase(all_wavelengths=np.asarray(source.wavelength_list), traced_rays = source.traced_rays, target_face = bs.faces.faces[0])
-    wavelength = 299792.458 / freq 
+    # wavelength = 299792.458 / freq 
 
+    wavelength, group_delay = evaluate_group_delay(all_wavelengths=np.asarray(source.wavelength_list), traced_rays = source.traced_rays, target_face = bs.faces.faces[0])
+    group_delay_fs = group_delay * 1000
+    wavelength = wavelength * 1000
     fig, ax = plt.subplots()
     ax.plot(wavelength, phase)
     ax.set(xlabel = "Wavelength (nm)", ylabel = "Phase", title = f"Phase Evalution, gd = {grating_distance}")
+    ax.grid()
+    now = time.strftime("%Y%m%d-%H%M%S")
+    fig.savefig(f"D:\senior_design\Screenshots\phase_plots\phase_plot_{now}.png")
+
+    meanSquaredError = ((group_delay_fs) ** 2).mean()
+    rmse = np.sqrt(meanSquaredError)
+    rmse = np.round(rmse, 2)
+
+    fig, ax = plt.subplots()
+    ax.plot(wavelength, group_delay_fs)
+    ax.set(xlabel = "Wavelength (nm)", ylabel = "Group Delay (fs)", title = f"Group Delay, rmse = {rmse} fs")
     ax.grid()
 
 
     # Get current date and time
     now = time.strftime("%Y%m%d-%H%M%S")
-    fig.savefig(f"D:\senior_design\Screenshots\phase_plots\phase_plot_{now}.png")
+    fig.savefig(f"D:\senior_design\Screenshots\group_delay_plots\group_delay_plot_{now}.png")
 except:
     print("Error making plot")
 
--- bryans_setups/grating_tilter_plot_maker.py
+++ bryans_setups/grating_tilter_plot_maker.py
@@ -11,7 +11,7 @@ from raypier.diffraction_gratings import RectangularGrating
 from raypier.mirrors import BaseMirror, PECMirror, RectMirror
 from raypier.sources import BroadbandRaySource
 from raypier.beamstop import BeamStop
-from raypier.results import GroupVelocityDispersion, MeanOpticalPathLength, evaluate_phase
+from raypier.results import GroupVelocityDispersion, MeanOpticalPathLength, evaluate_phase,evaluate_group_delay
 from raypier.constraints import BaseConstraint
 from raypier.chirp_result import ChirpResult
 from raypier.materials import OpticalMaterial
@@ -245,6 +245,96 @@ def generate_beam_lateral_pos_plot():
 
     fig.savefig(f"D:\senior_design\Screenshots\phase_plots\phase_eval_beam_pos_lateral.png")
 
+def generate_grating_distance_group_delay_plot():
+    all_rmse = []
+    grating_distances = np.arange(400, 990, 1)
+
+    direction = np.array([0.0,-1,-0.015])
+    grating_distance = 500
+    grating_init_rotation = -151.31
+    cylindrical_radius_curvature = 1000
+    source_bandwidth = 32
+    source_wavelength = 800
 
 
-generate_beam_lateral_pos_plot()
+    for grating_distance in grating_distances:
+
+        wavelength_start = source_wavelength / 1000 - source_bandwidth/2000
+        wavelength_end = source_wavelength / 1000 + source_bandwidth/2000
+        source = BroadbandRaySource(origin=(0,260,10),
+                                    direction=tuple(direction),
+                                    wavelength_start=wavelength_start,
+                                    wavelength_end = wavelength_end,
+                                    number=500,
+                                    uniform_deltaf=True,
+                                    max_ray_len=1000000.0)
+
+
+
+        grating = RectangularGrating(centre=(0,grating_distance,-4),
+                                    direction=(-1,0,0),
+                                    lines_per_mm=1200,
+                                    order=-1,
+                                    width = 10,
+                                    length = 30)
+
+
+        grating.orientation = grating_init_rotation
+
+        shape = RectangleShape(centre = (0, 0), width = 80, height = 60)
+        face2 = CylindericalFace(z_height=4.0, curvature=cylindrical_radius_curvature, mirror = True)
+        mat = OpticalMaterial(refractive_index=1)
+        mir = GeneralLens(name = "Cylinderincal Lens",
+                            centre = (0,0,0),
+                            direction=(0,-1,0),
+                            shape=shape, 
+                            surfaces=[face2], 
+                            materials=[mat])
+
+
+        bs = BeamStop(centre=(0,1000,-20),
+                    direction=(0 , -1,  0),
+                    diameter = 100
+                    )
+
+        gvd = GroupVelocityDispersion(source=source, target=bs.faces.faces[0])
+        chirp = ChirpResult(source=source, target=bs.faces.faces[0])
+        mean_optical_path = MeanOpticalPathLength(source = source, target = bs.faces.faces[0])
+
+        model = RayTraceModel(optics=[ grating, mir, bs], 
+                            sources=[source,],
+                            results=[gvd, chirp, mean_optical_path],
+                            #   constraints=[gvd_cstr,]
+                                )
+
+        try:
+            wavelength, group_delay = evaluate_group_delay(all_wavelengths=np.asarray(source.wavelength_list), traced_rays = source.traced_rays, target_face = bs.faces.faces[0])
+            group_delay_fs = group_delay * 1000
+            wavelength = wavelength * 1000
+
+
+            meanSquaredError = ((group_delay_fs) ** 2).mean()
+            rmse = np.sqrt(meanSquaredError)
+            all_rmse.append(rmse)
+
+            # fig, ax = plt.subplots()
+            # ax.plot(wavelength, group_delay_fs)
+            # ax.set(xlabel = "Wavelength (nm)", ylabel = "Group Delay (fs)", title = f"Group Delay, rmse = {rmse} fs")
+            # ax.grid()
+
+
+            # Get current date and time
+            # now = time.strftime("%Y%m%d-%H%M%S")
+            # fig.savefig(f"D:\senior_design\Screenshots\group_delay_plots\group_delay_plot_{now}.png")
+        except:
+            print("Error making plot")
+    
+    fig, ax = plt.subplots()
+    ax.plot(grating_distances, all_rmse)
+    ax.set(xlabel = "Grating Distance (mm)", ylabel = "Group Delay RMSE (fs)", title = f"Group Delay RMSE for varying grating distances, min = {grating_distances[np.argmin(all_rmse)]}")
+    ax.grid()
+    now = time.strftime("%Y%m%d-%H%M%S")
+    fig.savefig(f"D:\senior_design\Screenshots\group_delay_plots\group_delay_plot_grating_distances_{now}.png")
+
+
+generate_grating_distance_group_delay_plot()
